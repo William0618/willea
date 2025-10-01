f857392f708df779c10fc172bb4efc9ae69ccf80
CCJ1994 <a0changj2@gmail.com>
2025-10-01 09:48:30 +0800

    change color tone and layout

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-  gsap.to(".hero-bg", {
-    scale: 1.1,       // 放大倍數
-    ease: "power3.out", // 或 "power3.out"
-    scrollTrigger: {
-      trigger: ".hero-banner",
-      start: "top top",
-      end: "center top",
-      scrub: 1.2,
-      pin: true
-    }
-  });
+  // gsap.to(".hero-bg", {
+  //   scale: 1.1,       // 放大倍數
+  //   ease: "power3.out", // 或 "power3.out"
+  //   scrollTrigger: {
+  //     trigger: ".hero-banner",
+  //     start: "top top",
+  //     end: "center top",
+  //     scrub: 1.2,
+  //     pin: true
+  //   }
+  // });
 
   // let focus = document.querySelector(".focus");
 
@@ -24,18 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // });
   // 滑鼠移動視差 (hero 文字微偏移)
-  const heroContent = document.querySelector(".name-img");
-  document.addEventListener("mousemove", e => {
-    const { innerWidth, innerHeight } = window;
-    const moveX = (e.clientX - innerWidth / 2) / 50;  // 數字越小，偏移越大
-    const moveY = (e.clientY - innerHeight / 2) / 50;
-    gsap.to(heroContent, {
-      x: moveX,
-      y: moveY,
-      duration: 0.5,
-      ease: "power2.out"
-    });
-  });
+  // const heroContent = document.querySelector(".name-img");
+  // document.addEventListener("mousemove", e => {
+  //   const { innerWidth, innerHeight } = window;
+  //   const moveX = (e.clientX - innerWidth / 2) / 50;  // 數字越小，偏移越大
+  //   const moveY = (e.clientY - innerHeight / 2) / 50;
+  //   gsap.to(heroContent, {
+  //     x: moveX,
+  //     y: moveY,
+  //     duration: 0.5,
+  //     ease: "power2.out"
+  //   });
+  // });
   // gsap.to("#photo-gallery .gallery-img img", {
   //   y: -150,
   //   ease: "none",
@@ -82,8 +82,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // });
   let mm = gsap.matchMedia();
   // 桌機（>=768px）做 X 軸
+  gsap.to(".hero-bg", {
+    scale: 1.1,       // 放大倍數
+    ease: "power3.out", // 或 "power3.out"
+    scrollTrigger: {
+      trigger: "#hero",
+      start: "top top",
+      end: "center top",
+      scrub: 1.2,
+      pin: true
+    }
+  });
   mm.add("(min-width: 768px)", () => {
-    gsap.from(".groom-part", {
+    gsap.from(".bride-part", {
       xPercent: 100,
       rotate: 0,
       opacity: 0,
@@ -91,14 +102,14 @@ document.addEventListener("DOMContentLoaded", () => {
         scroller: "body",
         start: "top 100%",
         end: "top 0",
-        trigger: ".groom-part",
+        trigger: ".bride-part",
         scrub: 2,
         // markers: true,
 
       }
     });
 
-    gsap.from(".bride-part", {
+    gsap.from(".groom-part", {
       xPercent: -100,
       rotate: 0,
       opacity: 0,
@@ -106,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
         scroller: "body",
         start: "top 100%",
         end: "top 0%",
-        trigger: ".bride-part",
+        trigger: ".groom-part",
         scrub: 2,
         // markers: true,
 
@@ -118,11 +129,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 手機（<768px）做 Y 軸
   mm.add("(max-width: 767px)", () => {
-    gsap.from(".groom-part .intro-text", {
+    gsap.from(".bride-part .intro-text", {
       y: 80,
       opacity: 0,
       scrollTrigger: {
-        trigger: ".groom-part .intro-text",
+        trigger: ".bride-part .intro-text",
         start: "top 100%",
         end: "top 0%",
         scrub: 2,
@@ -130,11 +141,11 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     });
 
-    gsap.from(".bride-part .intro-text", {
+    gsap.from(".groom-part .intro-text", {
       y: -80,
       opacity: 0,
       scrollTrigger: {
-        trigger: ".bride-part .intro-text",
+        trigger: ".groom-part .intro-text",
         start: "top 100%",
         end: "top 50%",
         scrub: 2,
@@ -142,5 +153,39 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-  
+  const imageOne = document.querySelector("#invitation .image1")
+
+  imageOne.addEventListener("mouseenter", function () {
+    imageOne.style.scale = "2"
+    imageOne.style.transform = "rotate(-10deg)"
+  });
+
+  imageOne.addEventListener("mouseleave", function () {
+    imageOne.style.scale = "1"
+    imageOne.style.transform = "rotate(0deg)"
+  });
+
+  const imageTwo = document.querySelector("#invitation .image2")
+
+
+  imageTwo.addEventListener("mouseenter", function () {
+    imageTwo.style.scale = "2"
+    imageTwo.style.transform = "rotate(10deg)"
+  });
+
+  imageTwo.addEventListener("mouseleave", function () {
+    imageTwo.style.scale = "1"
+    imageTwo.style.transform = "rotate(0deg)"
+  });
+  const imageThree = document.querySelector("#invitation .image3")
+
+  imageThree.addEventListener("mouseenter", function () {
+    imageThree.style.scale = "2"
+    imageThree.style.transform = "rotate(-10deg)"
+  });
+
+  imageThree.addEventListener("mouseleave", function () {
+    imageThree.style.scale = "1"
+    imageThree.style.transform = "rotate(0deg)"
+  });
 });
